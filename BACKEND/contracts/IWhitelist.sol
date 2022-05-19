95cfb276ea8c9cf94cfa24973f0541c561c8d981
SahilPanhotra <sahilpanhotra@gmail.com>
2022-05-19 20:18:21 +0530

    added Frontend

--- BACKEND/contracts/IWhitelist.sol
+++ BACKEND/contracts/IWhitelist.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.4;
 
 interface IWhitelist {
 
-    function WhitlistedAddresses(address) external view returns(bool);
+    function whitelistedAddresses(address) external view returns(bool);
     
 }
